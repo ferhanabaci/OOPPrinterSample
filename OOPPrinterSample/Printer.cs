a3757622ef6f94d181a9c0d85ac9bd938cfd45f1
Ferhan Abacı <84642053+ferhanabaci@users.noreply.github.com>
2022-07-04 15:16:41 +0300

    Add project files.

---
+++ OOPPrinterSample/Printer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPrinterSample
+{
+    internal class Printer
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        //public bool IsOpen { get; set; }
+        public Status status { get; set; }
+    }
+}
---
+++ OOPPrinterSample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPPrinterSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
